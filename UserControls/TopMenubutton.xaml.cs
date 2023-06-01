c5eb4d86c6eefbd916f4a786c318233de707e37f
Ben Grob <ben.grob@stud.kftg.ch>
2023-06-01 02:28:28 +0200

    a

---
+++ UserControls/TopMenubutton.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.IconPacks;
+
+
+namespace Project_C14.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für MenuButton.xaml
+    /// </summary>
+    public partial class TopMenubutton : UserControl
+    {
+        public TopMenubutton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(TopMenubutton));
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(TopMenubutton));
+    }
+}
