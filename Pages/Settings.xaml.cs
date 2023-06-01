0a9cd29d5084bdf33ce696520bc882a5028ae883
Ben Grob <ben.grob@stud.kftg.ch>
2023-06-01 04:57:26 +0200

    h

--- Pages/Settings.xaml.cs
+++ Pages/Settings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Project_C14;
@@ -11,9 +12,20 @@ public partial class Settings : Page
         InitializeComponent();
     }
 
-    private void ToggleThemeButton_Click(object sender, RoutedEventArgs e)
+    public void ThemeToggleButton_Click(object sender, RoutedEventArgs e)
     {
-        
+        if (ThemeToggleButton.IsChecked == true)
+        {
+            // Dark theme is selected
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("Themes/Dark.xaml", UriKind.Relative) });
+        }
+        else
+        {
+            // Light theme is selected
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("Themes/Light.xaml", UriKind.Relative) });
+        }
     }
 
 
